47f4f6557763214458c94d34b086c860dc63f80f
[David <103580905+codeAdventureNow@users.noreply.github.com>
2023-07-29 10:38:53 -0700

    new nav bar version 2

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,12 +1,15 @@
 export default function Nav() {
   return (
-    <nav>
+    <header>
       <h2 className='logo'>
+        <i className='bx bx-hard-hat'></i>
         Nate <span className='accent-color'>Mueller</span>{' '}
       </h2>
-      <ul>
+      <ul className='navlist'>
         <li>
-          <a href='#'>Projects</a>
+          <a href='#' className='active'>
+            Projects
+          </a>
         </li>
         <li>
           <a href='#'>About</a>
@@ -15,7 +18,13 @@ export default function Nav() {
           <a href='#'>Contact</a>
         </li>
       </ul>
-      <button type='button'>Resume</button>
-    </nav>
+      <div className='h-main'>
+        <a href='#' className='h-btn'>
+          Resume
+        </a>
+        <div className='bx bx-menu' id='menu-icon'></div>
+        <div className='bx bx-moon' id='darkmode'></div>
+      </div>
+    </header>
   );
 }
